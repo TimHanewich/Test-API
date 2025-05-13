81a893e83a9f35c20b9a8839de8958dbcfc3fcc6
Tim Hanewich <tahanewich@live.com>
2025-05-13 08:16:15 -0400

    Use all URL's

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,7 @@ namespace TestAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.WebHost.UseUrls("http://0.0.0.0:5000");
             builder.Services.AddControllers();
             var app = builder.Build();
             app.MapControllers();
