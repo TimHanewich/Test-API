413d3e143b980bd8e976ccb92dc3a0a131de1b5e
Tim Hanewich <tahanewich@live.com>
2025-05-13 12:49:09 -0400

    Set up simple data server

--- src-python-flask/app.py
+++ src-python-flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+import json
 
 app = Flask("my_api")
 
@@ -17,6 +18,49 @@ def hello():
         r.set_data("I got this from your POST request: '" + data + "'")
         r.headers["Content-Type"] = "text/plain"
         return r
+    
+
+_data:list[str] = []
+@app.route("/data", methods=["GET", "POST", "DELETE"])
+def data() -> Response:
+    if request.method.upper() == "GET":
+        r = Response()
+        r.status = 200
+        r.headers["Content-Type"] = "application/json"
+        r.set_data(json.dumps(_data))
+        return r
+    elif request.method.upper() == "POST":
+
+        # get the body
+        body:str = request.get_data(as_text=True)
+        _data.append(body)
+
+        # return successful
+        r = Response()
+        r.status = 201
+        return r
+    elif request.method.upper() == "DELETE":
+
+        # get the body
+        body:str = request.get_data(as_text=True)
+
+        # is inside?
+        if body not in _data:
+            r = Response()
+            r.status = 404
+            r.headers["Content-Type"] = "plain/text"
+            r.set_data("Could not find data point '" + body + "' in array.")
+            return r
+
+        # remove
+        _data.remove(body)
+
+        # return success
+        r = Response()
+        r.status = 200
+        return r
+
+    
     
 
 app.run(host='0.0.0.0', port=5000)
