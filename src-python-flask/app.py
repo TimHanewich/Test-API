e7dc0f937b756d70a3a400a93398a466e3d26a37
Tim Hanewich <tahanewich@live.com>
2025-05-13 12:39:42 -0400

    Fixed declaration of methods

--- src-python-flask/app.py
+++ src-python-flask/app.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, Response
 
 app = Flask("my_api")
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=["GET", "POST"])
 def hello():
-    if request.method == 'POST':
+    if request.method.upper() == 'POST':
         data:str = request.get_data(as_text=True)
         r = Response()
         r.status = 200
         r.set_data("I got this from you: '" + data + "'")
         r.headers["Content-Type"] = "text/plain"
         return r
-    elif request.method == 'GET':
+    elif request.method.upper() == 'GET':
         r = Response()
         r.status = 200
         r.set_data("{\"message\": \"hi! Thanks for the get request!\"}")
