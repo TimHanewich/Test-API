ff5b64c671315da114cf8070088c2ba2a389d9c9
Tim Hanewich <tahanewich@live.com>
2025-05-13 12:40:17 -0400

    Rearranged

--- src-python-flask/app.py
+++ src-python-flask/app.py
@@ -4,18 +4,19 @@ app = Flask("my_api")
 
 @app.route('/hello', methods=["GET", "POST"])
 def hello():
-    if request.method.upper() == 'POST':
+    if request.method.upper() == 'GET':
+        r = Response()
+        r.status = 200
+        r.set_data("{\"message\": \"hi! Thanks for the GET request!\"}")
+        r.headers["Content-Type"] = "application/json"
+        return r
+    elif request.method.upper() == 'POST':
         data:str = request.get_data(as_text=True)
         r = Response()
         r.status = 200
-        r.set_data("I got this from you: '" + data + "'")
+        r.set_data("I got this from your POST request: '" + data + "'")
         r.headers["Content-Type"] = "text/plain"
         return r
-    elif request.method.upper() == 'GET':
-        r = Response()
-        r.status = 200
-        r.set_data("{\"message\": \"hi! Thanks for the get request!\"}")
-        r.headers["Content-Type"] = "application/json"
-        return r
+    
 
 app.run(host='0.0.0.0', port=5000)
