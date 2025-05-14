3782418db0d166af950a6807e52141b6dd600e11
Tim Hanewich <tahanewich@live.com>
2025-05-13 21:51:35 -0400

    Used DataStore as singleton

--- src/SayHi.cs
+++ src/SayHi.cs
@@ -56,5 +56,59 @@ namespace MyNamespace.Controllers
             //Add it
             _ds.Add(jo);
         }
+
+        //delete
+        [HttpDelete]
+        public async Task Delete()
+        {
+            //Check correct header
+            string? contenttype = Request.Headers["Content-Type"];
+            if (contenttype == null || contenttype != "application/json")
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("You must provide data in JSON! Please ensure the data you provide is in JSON format and you specified the Content-Type header as application/json.");
+                return;
+            }
+
+            //Get out body
+            StreamReader sr = new StreamReader(Request.Body);
+            string body = await sr.ReadToEndAsync();
+            JObject jo;
+            try
+            {
+                jo = JObject.Parse(body);
+            }
+            catch
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("There was an issue parsing the JSON out of the body. You must provide valid JSON!");
+                return;
+            }
+
+            //Find an object in the array that looks just like this one!
+            JObject? ToDelete = null;
+            foreach (JObject ptd in _ds.Data)
+            {
+                if (ptd.ToString() == jo.ToString())
+                {
+                    ToDelete = ptd;
+                }
+            }
+
+            //not found?
+            if (ToDelete == null)
+            {
+                Response.StatusCode = 404;
+                await Response.WriteAsync("Unable to find the object you provided in the data.");
+                return;
+            }
+
+            //Delete it
+            _ds.Remove(ToDelete);
+
+            //Success!
+            Response.StatusCode = 204;
+        }
+
     }
 }
