46f7e65bdaf06b9ecb62b06cee3a3b95f1d0fc03
Tim Hanewich <tahanewich@live.com>
2025-05-13 21:45:50 -0400

    Added datastore as singleton

--- src/Program.cs
+++ src/Program.cs
@@ -6,9 +6,16 @@ namespace TestAPI
     {
         public static void Main(string[] args)
         {
+            //Set up builder
             var builder = WebApplication.CreateBuilder(args);
             builder.WebHost.UseUrls("http://0.0.0.0:80");
             builder.Services.AddControllers();
+
+            //Add datastore as singleton
+            DataStore ds = new DataStore();
+            builder.Services.AddSingleton(ds);
+
+            //Run the app
             var app = builder.Build();
             app.MapControllers();
             app.Run();
--- src/SayHi.cs
+++ src/SayHi.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TestAPI;
 
 namespace MyNamespace.Controllers
 {
@@ -7,18 +10,51 @@ namespace MyNamespace.Controllers
     [Route("hi")]
     public class SayHi : ControllerBase
     {
+
+        private DataStore _ds;
+
+        public SayHi(DataStore ds)
+        {
+            _ds = ds;
+        }
+
         [HttpGet]
         public async Task Get()
         {
             Response.StatusCode = 200;
             Response.Headers.Append("Content-Type", "application/json");
-            await Response.WriteAsync("{\"name\": \"tim\"}");
+            await Response.WriteAsync(_ds.Data.ToString());
         }
 
         [HttpPost]
-        public IActionResult Post()
+        public async Task Post()
         {
-            return Ok("Thanks for the post!");
+            //Check correct header
+            string? contenttype = Request.Headers["Content-Type"];
+            if (contenttype == null || contenttype != "application/json")
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("You must provide data in JSON! Please ensure the data you provide is in JSON format and you specified the Content-Type header as application/json.");
+                return;
+            }
+
+            //Get out body
+            StreamReader sr = new StreamReader(Request.Body);
+            string body = await sr.ReadToEndAsync();
+            JObject jo;
+            try
+            {
+                jo = JObject.Parse(body);
+            }
+            catch
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("There was an issue parsing the JSON out of the body. You must provide valid JSON!");
+                return;
+            }
+
+            //Add it
+            _ds.Add(jo);
         }
     }
 }
